c1625f34f45b18e54716392cf9232726aff40902
Kevin Guerrero <kevinguerrerop1@gmail.com>
2024-05-31 16:35:13 -0400

    Cambio estilo grafico

--- resources/views/Tickets/charts.blade.php
+++ resources/views/Tickets/charts.blade.php
@@ -26,14 +26,24 @@
     const ctx = document.getElementById('myChart');
 
     new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: {!! json_encode($months) !!},
-        datasets: [{
-          label: 'Total de Tickets por Mes',
-          data: {!! json_encode($monthCount) !!},
-          borderWidth: 1
-        }]
+        type: 'line',
+  data: {
+    labels: {!! json_encode($months) !!},
+    datasets: [{
+        label: 'Total de Tickets por Mes',
+        lineTension: 0.3,
+        backgroundColor: "rgba(78, 115, 223, 0.05)",
+        borderColor: "rgba(78, 115, 223, 1)",
+        pointRadius: 3,
+        pointBackgroundColor: "rgba(78, 115, 223, 1)",
+        pointBorderColor: "rgba(78, 115, 223, 1)",
+        pointHoverRadius: 3,
+        pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+        pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+        pointHitRadius: 10,
+        pointBorderWidth: 2,
+        data: {!! json_encode($monthCount) !!},
+    }],
       },
       options: {
         scales: {
